816a648817d78ae04524b1728a5880eef8d31d5f
ekla <barlavigabriel@gmail.com>
2023-01-03 21:40:39 +0100

    slight improvements with user input for number of pages and display of remaining pages, and in the end the total number of files downloaded. Preparing fork to add a progress bar.

--- WebSiteScrapping.py
+++ WebSiteScrapping.py
@@ -3,8 +3,25 @@ from bs4 import BeautifulSoup
 from threading import Thread
 import requests
 
-with open('list.txt','r') as f_keywordlist:
-    key_word_list = f_keywordlist.read().splitlines()
+
+def myprint(text):
+    return print(text, flush=True)
+
+key_word = input('enter a key-word or press enter:\n#>')
+number_of_page = input('enter the number of page you want (80 images/page):\n#>')
+number_of_page = int(number_of_page)
+if key_word == '' or key_word == '\n':
+    with open('list.txt','r') as f_keywordlist:
+        key_word_list = f_keywordlist.read().splitlines()
+else:
+    key_word_list = [key_word]
+
+if '+' in key_word:
+    dirname = key_word.replace('+','-')
+    filename = dirname
+else:
+    dirname = key_word
+    filename = key_word
 
 j = 0
 pages_for_keyword = ('?','?')
@@ -13,7 +30,8 @@ key_words_number = len(key_word_list)
 for key_word in key_word_list:
 
     k = 0
-    for n in range(2, 20):
+    number_of_files = 0
+    for n in range(1, number_of_page):
         
         links_for_download = []
         myreq_get = requests.get(f'https://www.wallpaperflare.com/search?wallpaper={key_word}&&page={n}') 
@@ -21,7 +39,7 @@ for key_word in key_word_list:
             print(f"No more page for this key word ({key_word}),\nseems like we've been throught all existing pages...")
             print(f"all images from key word {key_word} should have been dowloaded successfully.")
             print("done !")
-            print(f'folder {key_word} has been successfully filled out. {j+1}/{key_words_number}')
+            print(f'folder {dirname} has been successfully filled out. {j+1}/{key_words_number}')
             print(f'{key_words_number-j-1} remaining...')
             j += 1
             break
@@ -30,9 +48,9 @@ for key_word in key_word_list:
         
             soup = BeautifulSoup(html_text, 'lxml')
             job = soup.find_all('a', {"itemprop": 'url'})
-            if not os.path.isdir(f'img/{key_word}'):
-                os.mkdir(f'img/{key_word}')
-                print(f'folder {key_word} created')
+            if not os.path.isdir(f'img/{dirname}'):
+                os.mkdir(f'img/{dirname}')
+                print(f'folder {dirname} created')
             for link in job:
                 # print(link.attrs["href"])
                 link = link.attrs["href"] + "/download"    
@@ -45,26 +63,30 @@ for key_word in key_word_list:
                 job = soup.find_all(id = "show_img")
                 for link in job:
                     url_img = link.attrs['src']
-                    if not os.path.isfile(f'img/{key_word}/{key_word}-{i}-page{n}.jpg'):
+                    if not os.path.isfile(f'img/{dirname}/{filename}-{i}-page{n}.jpg'):
                         print(f"Downloading file from: {url_img}")
                         get_content = requests.get(url_img,stream=True)
-                        with open(f'img/{key_word}/{key_word}-{i}-page{n}.jpg','wb') as fd:
+                        with open(f'img/{dirname}/{filename}-page{n}-IMG{i}.jpg','wb') as fd:
                             for chunk in get_content.iter_content(chunk_size=512):
                                 fd.write(chunk)
                             fd.close()
                         print(f"img file:{fd.name} copied\n {i}/{len(links_for_download)} : {len(links_for_download)-i} remaining")
+                        print(f"page {n}/{number_of_page} : {number_of_page-n} remaining")
                     else:
-                        print(f"the file img/{key_word}/{key_word}-{i}-page{n}.jpg already exists.")
+                        print(f"the file img/{dirname}/{filename}-page{n}-{i}.jpg already exists.")
                         print(f"skipping this url...")
                 i+=1
+                number_of_files+=1
             print("===========================================================")
-            print(f'Page {n} has been scrapped entirely in folder {key_word}.')
+            print(f'Page {n} has been scrapped entirely in folder {dirname}.')
+            print("===========================================================")
+            print(f' still {number_of_page-n} to scrap remaining...')
             print("===========================================================")
             print(f'Scraping of page {n+1} started...')
             print("==================================")
             k+=1
     print("Done !")
-    pages_for_keyword = key_word, f'{k} pages'
+    pages_for_keyword = key_word, f'{k} pages, {number_of_files} image files'
     summary.append(pages_for_keyword)
 print('Job Terminated successfully.')
 print('number of pages for each key word is :')
@@ -75,6 +97,7 @@ sys.exit(0)
 
 
 def scrapping_iteration():
+    pass
 
 
                 
--- scrapClass.py
+++ scrapClass.py
@@ -1,2 +1,13 @@
 class Scrap():
 
+    def __init__(self) -> None:
+        self.baseurl = ''
+        self.download_URL = ''
+        self.keywords = []
+        self.links = ''
+    
+    def init_variables():
+        pass
+    
+
+
